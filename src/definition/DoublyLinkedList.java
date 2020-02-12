bcd860fcb76bf5e3157dc35b471781903ed64902
dbc2201 <divyanshb22@gmail.com>
2020-02-13 05:22:02 +0530

    implement the List ADT and methods

--- src/definition/DoublyLinkedList.java
+++ src/definition/DoublyLinkedList.java
@@ -7,5 +7,26 @@
 
 package definition;
 
-public class DoublyLinkedList<E> {
+import adt.ListADT;
+
+public class DoublyLinkedList<E> implements ListADT<E> {
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
 }
