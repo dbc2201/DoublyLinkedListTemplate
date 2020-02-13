3e0f05cc9f6a8bd49e88216fd6b863f50af286a8
dbc2201 <divyanshb22@gmail.com>
2020-02-13 05:40:46 +0530

    print the empty list

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,5 +12,13 @@ import definition.DoublyLinkedList;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+
+        /*
+         * The list is currently empty, let's print it, it would show
+         * []
+         * Remember, doing this is only possible because we had overridden the
+         * toString() method in the DoublyLinkedList class.
+         * */
+        System.out.println(list);
     }
 }
